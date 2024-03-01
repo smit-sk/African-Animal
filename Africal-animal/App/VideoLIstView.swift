435418fffa01e8ebf9c0c2ade9951ab55fba3bd1
smit-sk <patelsmit7008@gmail.com>
2024-03-01 18:00:09 -0500

    added-video-player

--- Africal-animal/App/VideoLIstView.swift
+++ Africal-animal/App/VideoLIstView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct VideoListView: View {
+    
+    @State var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List{
+                ForEach(videos) { item in
+                    NavigationLink(destination: VideoPlayerView(videoSelected: item.id, videoTitle: item.name)){
+                        VideoListItem(video: item)
+                            .padding(.vertical, 8)
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+            .toolbar{
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Button(action: {
+                        videos.shuffle ()
+                        hapticImpact.impactOccurred()
+                    }){
+                        Image(systemName: "arrow.2.squarepath")
+                    }
+                }
+            }
+        } //: NAVIGATION VIEW
     }
 }
 
--- Africal-animal/View/VideoListItem.swift
+++ Africal-animal/View/VideoListItem.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct VideoListItem: View {
+    let video: Video 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:10) {
+            ZStack {
+                Image(video.thumbnail)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 80)
+                .cornerRadius(9)
+                
+                Image(systemName: "play.circle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:32)
+                    .shadow(radius: 4)
+                
+                }
+            
+            VStack(alignment: .leading, spacing: 20){
+                Text(video.name)
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                    
+                Text(video.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+        }//: HSTACK
     }
 }
 
-#Preview {
-    VideoListItem()
+struct VideoListItem_Previews : PreviewProvider {
+    static let video: [Video] = Bundle.main.decode("videos.json")
+    static var previews: some View {
+        VideoListItem(video: video[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
 }

ae32b8beba87c730b786fe561d4f583fed6784e2
smit-sk <patelsmit7008@gmail.com>
2024-03-01 19:10:33 -0500

    Mapkit - Custom Annotation - Marker -PIN

---
+++ Africal-animal/Model/LocationModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationModel.swift
+//  Africal-animal
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-01.
+//
+
+import Foundation
---
+++ Africal-animal/View/MapAnnotationView.swift
@@ -0,0 +1,18 @@
+//
+//  MapAnnotationView.swift
+//  Africal-animal
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-01.
+//
+
+import SwiftUI
+
+struct MapAnnotationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapAnnotationView()
+}

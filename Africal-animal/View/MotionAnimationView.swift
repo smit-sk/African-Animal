dcaecf29d251c7cea044dbf649da1e0930de3804
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-02 19:54:17 -0500

    GridLayout-RandomCircleAnimation

---
+++ Africal-animal/View/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Africal-animal
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-02.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}

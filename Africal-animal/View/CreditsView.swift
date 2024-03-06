2c593a2be5c292d2fcaaf80f8167d139f780095d
smitkumar.khokhariya <smitkumar.khokhariya@corsameets.com>
2024-03-05 19:46:46 -0500

    Modifiers, iMessageIcons, GridViewStyle

---
+++ Africal-animal/View/CreditsView.swift
@@ -0,0 +1,18 @@
+//
+//  CreditsView.swift
+//  Africal-animal
+//
+//  Created by SMIT KHOKHARIYA on 2024-03-05.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreditsView()
+}
